f3f45174982f1386f83242c355816d898cdaa199
hassan boudraa <hassan.hbmama@example.com>
2023-11-26 09:05:31 -0800

    instal pycage

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Ensure the package is present with the specified version
-package { 'flask':
+# 1-install_a_package.pp
+
+# Ensure the package is present and at the specified version
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
+# Notify that the package installation is completed
+notify { 'Flask installed':
+  require => Package['Flask'],
+}
