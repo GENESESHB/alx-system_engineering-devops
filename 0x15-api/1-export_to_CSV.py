97ee3303c651a537a7cbcd37db49341f62a5bb2b
root <root@DESKTOP-MOC38TV>
2024-01-01 17:58:46 +0100

    add tasks 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+this is a script to export data in the CSV format 
+Requirements:
+            - Records all tasks that are owned by this employee.
+                    - Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS",
+                            "TASK_TITLE".
+                                    - File name must be: USER_ID.csv.
+"""
+
+import csv
+from os import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+    usrname = (requests.get(f"{url}users/{user_id}").json().get("username"))
+    todos = (requests.get(f"{url}user/{user_id}/todos").json())
+    tasks = list(todos)
+
+     csv_filename = f"{user_id}.csv"
+     with open(csv_filename, "w") as csvfile:
+         csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+         for task in tasks:
+             csv_writer.writerow([user_id, usrname,
+                 task['completed'],
+                 task['title']])
