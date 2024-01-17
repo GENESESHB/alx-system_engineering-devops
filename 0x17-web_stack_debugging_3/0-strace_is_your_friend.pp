4b5bc869a5c90a522565b00d34fbc664b787a1f9
root <root@DESKTOP-MOC38TV>
2024-01-17 16:16:34 +0100

    add new requirement for task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,23 +1,6 @@
-# 0-strace_is_your_friend.pp
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-# Define an exec resource to run strace on the Apache process
-exec { 'strace_apache':
-  command     => 'strace -o /tmp/strace_output.txt -f -p $(pgrep apache)',
-  path        => '/usr/bin:/bin',
-  refreshonly => true,
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
-
-# Define an exec resource to fix the identified issue
-exec { 'fix_apache_issue':
-  command     => 'your_command_to_fix_the_issue', # Replace with the actual command
-  path        => '/usr/bin:/bin',
-  subscribe   => Exec['strace_apache'], # Only run this after strace completes
-  refreshonly => true,
-}
-
-# Notify the user about the success
-notify { 'Apache_fixed':
-  message => 'Apache issue fixed successfully!',
-  require => Exec['fix_apache_issue'],
-}
-
