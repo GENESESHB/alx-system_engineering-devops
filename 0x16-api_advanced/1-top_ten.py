a80436520e3b6670443de18e05a8a579add9f6a9
root <root@DESKTOP-MOC38TV>
2024-01-18 02:18:13 +0100

    fixing outpUt

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit."""
+"""Function to print hot posts on a given Reddit subreddit."""
+import requests
 
 
 def top_ten(subreddit):
-    """function that queries the Reddit API and prints the titles
-    of the first 10 hot posts listed for a given subreddit."""
-    import requests
-    import sys
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for i in range(10):
-                print(children[i].get('data').get('title'))
-        else:
-            print("None")
-    except Exception:
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            }
+    params = {
+            "limit": 10
+            }
+    response = requests.get(url, headers=headers, params=params,
+            allow_redirects=False)
+    if response.status_code == 404:
         print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
