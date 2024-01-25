a69fd65adf9ebf4329f83a40f4b227f02b893a58
root <root@DESKTOP-MOC38TV>
2024-01-25 14:02:29 +0100

    add new directory and readme

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,11 +3,11 @@
 # Increase hard file limit for holberton user
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+  path    => '~/usr/local/bin/:/bin/',
 }
 
 #Increase soft file limit for user holberton
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+  path    => '~/usr/local/bin/:/bin/',
 }
